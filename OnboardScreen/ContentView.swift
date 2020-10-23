1173ab86081c57049fc2f152360efb9581c4d757
rick2785 <rjhrabowskie@gmail.com>
2020-10-23 15:26:35 -0500

    Initial Commit

---
+++ OnboardScreen/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OnboardScreen
+//
+//  Created by RJ Hrabowskie on 10/23/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OnboardScreen/OnboardScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardScreenApp.swift
+//  OnboardScreen
+//
+//  Created by RJ Hrabowskie on 10/23/20.
+//
+
+import SwiftUI
+
+@main
+struct OnboardScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
